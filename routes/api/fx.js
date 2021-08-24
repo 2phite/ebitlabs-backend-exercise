7d744cb61fe1258c40420a824f9e9be3c7006214
2phite <2phite@gmail.com>
2021-08-24 17:21:26 +0100

    Return the most recent ETH/USD price

--- routes/api/fx.js
+++ routes/api/fx.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const sqlite3 = require('sqlite3').verbose()
+const db_path = './db.sqlite3'
 
 router.get('/ohlc/ETHUSD', (req, res) => {
-  // Hard code
-  res.json({"pair": "ETH/USD", "vwap": 1234.5})
+  let db = new sqlite3.Database(db_path, (err) => {
+    if (err) {
+      console.error(err.message)
+    }
+    console.log('Connected to the file database.')
+
+    db.serialize(() => {
+      let sql = `SELECT pair, vwap
+        FROM market_price
+        WHERE pair = 'ETH/USD'
+        ORDER BY startTime DESC`
+      db.get(sql, [], (err, row) => {
+        if (err) {
+          console.error(err.message)
+          res.status(404)
+          res.json({'msg': 'Data not found'})
+        } else {
+          console.log(row)
+          res.json(row)
+        }
+      })
+    })
+
+    db.close((err) => {
+      if (err) {
+        console.error(err.message)
+      }
+      console.log('Close the database connection.')
+    })
+  })
 })
 
 module.exports = router
