52ce9bfa02b2dbbae583d3f104463602ca0a2f76
2phite <2phite@gmail.com>
2021-08-24 17:40:20 +0100

    Also retrieve the ETH/GBP price

--- routes/api/fx.js
+++ routes/api/fx.js
@@ -2,8 +2,20 @@ const express = require('express')
 const router = express.Router()
 const sqlite3 = require('sqlite3').verbose()
 const db_path = './db.sqlite3'
+const pair_dict = {'ETHGBP': 'ETH/GBP', 'ETHUSD': 'ETH/USD'}
 
-router.get('/ohlc/ETHUSD', (req, res) => {
+router.get('/ohlc/:pair', (req, res) => {
+  let pair = undefined
+  if (pair_dict[req.params.pair]) {
+    console.log(pair_dict[req.params.pair])
+    pair = pair_dict[req.params.pair]
+  } else {
+    console.log('Pair name not found')
+    res.status(404)
+    res.json({'msg': 'Pair name not found'})
+    return
+  }
+
   let db = new sqlite3.Database(db_path, (err) => {
     if (err) {
       console.error(err.message)
@@ -13,9 +25,9 @@ router.get('/ohlc/ETHUSD', (req, res) => {
     db.serialize(() => {
       let sql = `SELECT pair, vwap
         FROM market_price
-        WHERE pair = 'ETH/USD'
+        WHERE pair = ?
         ORDER BY startTime DESC`
-      db.get(sql, [], (err, row) => {
+      db.get(sql, [pair], (err, row) => {
         if (err) {
           console.error(err.message)
           res.status(404)
